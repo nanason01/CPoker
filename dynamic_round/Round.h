274b37331fc085098d928eafe679b18c6066e728
nanason01 <nanason@umich.edu>
2022-12-07 16:51:33 -0500

    done debugging, seems to work

--- dynamic_round/Round.h
+++ dynamic_round/Round.h
@@ -12,6 +12,12 @@
 
 // *** All utilities defined in terms of p1's gain ***
 
+#ifdef DEBUG
+#define dprint printf
+#else
+#define dprint(...)
+#endif
+
 constexpr int MAX_CHILD_STATES = 5;
 constexpr int NUM_HANDS = 10;
 constexpr float REGRET_INIT = 0.10;
@@ -45,30 +51,32 @@ struct InfoSets {
     std::array<float, MAX_CHILD_STATES> terminal_util;
 
     inline float& get_regr(const int card, const int child_idx) {
-        return sum_child_regr[card][child_idx];
+        return sum_child_regr[ card ][ child_idx ];
     }
 
     // needed to default initialize arrays
-    InfoSets() : num_children(0) {
+    InfoSets(bool _is_p1): is_p1(_is_p1), num_children(0) {
         for (int card = 0; card < NUM_HANDS; card++)
-            sum_child_regr[card].fill(REGRET_INIT);
+            sum_child_regr[ card ].fill(REGRET_INIT);
         children.fill(nullptr);
         child_states.fill(Type::Invalid);
     }
 
     void add_showdown(float util) {
-        child_states[num_children] = Type::Showdown;
-        terminal_util[num_children++] = util;
+        assert(util > 0.0);
+        child_states[ num_children ] = Type::Showdown;
+        terminal_util[ num_children++ ] = util;
     }
 
     void add_fold(float util) {
-        child_states[num_children] = Type::Fold;
-        terminal_util[num_children++] = util;
+        assert(util > 0.0);
+        child_states[ num_children ] = Type::Fold;
+        terminal_util[ num_children++ ] = util;
     }
 
     void add_child(InfoSets* child) {
-        child_states[num_children] = Type::Continue;
-        children[num_children++] = child;
+        child_states[ num_children ] = Type::Continue;
+        children[ num_children++ ] = child;
     }
 
     float get_util(int card1, int card2, bool p1_wins, float prob_in) {
@@ -81,24 +89,24 @@ struct InfoSets {
         }
 
         // util is a weighted avg of child utilities
-        float util;
+        float util = 0.0;
         std::array<float, MAX_CHILD_STATES> child_utils;
-        for (int child_idx = 0; child_states[child_idx] != Type::Invalid; child_idx++) {
+        for (int child_idx = 0; child_states[ child_idx ] != Type::Invalid; child_idx++) {
             const float strat_prob = get_regr(info_state_card, child_idx) / sum_regr;
 
-            switch (child_states[child_idx]) {
+            switch (child_states[ child_idx ]) {
             case Type::Continue:
-                child_utils[child_idx] = children[child_idx]->get_util(card1, card2, p1_wins, prob_in * strat_prob);
+                child_utils[ child_idx ] = children[ child_idx ]->get_util(card1, card2, p1_wins, prob_in * strat_prob);
                 break;
             case Type::Fold:
-                child_utils[child_idx] = terminal_util[child_idx] * (is_p1 ? -1.0 : 1.0);
+                child_utils[ child_idx ] = terminal_util[ child_idx ] * (is_p1 ? -1.0 : 1.0);
                 break;
             case Type::Showdown:
-                child_utils[child_idx] = terminal_util[child_idx] * (p1_wins ? 1.0 : -1.0);
+                child_utils[ child_idx ] = terminal_util[ child_idx ] * (p1_wins ? 1.0 : -1.0);
                 break;
             }
 
-            util += child_utils[child_idx] * strat_prob;
+            util += child_utils[ child_idx ] * strat_prob;
         }
 
         // update sum of regrets
@@ -111,8 +119,8 @@ struct InfoSets {
         // p1 and p2 have opposite perspectives, so p1 is trying to maximize util
         // whereas p2 is trying to minimize it.
         // So, p1 will update non-negative regrets whereas p2 will update non-positive regrets
-        for (int child_idx = 0; num_children; child_idx++) {
-            const float regr = child_utils[child_idx] - util;
+        for (int child_idx = 0; child_idx < num_children; child_idx++) {
+            const float regr = child_utils[ child_idx ] - util;
 
             if (is_p1)
                 get_regr(info_state_card, child_idx) += regr > 0.0 ? regr * prob_in : 0.0;
@@ -128,10 +136,15 @@ struct InfoSets {
         int sz = sizeof(InfoSets);
 
         for (int child_idx = 0; num_children; child_idx++) {
-            sz += children[child_idx]->size_bytes();
+            sz += children[ child_idx ]->size_bytes();
         }
 
         return sz;
+    }
+
+    // 
+    float get_mes(bool is_p1) {
+
     }
 
     // prints the strategy in this case
@@ -146,7 +159,7 @@ struct InfoSets {
         if (history == "")
             cout << "To open: \n";
         else
-            cout << "Facing " << history << ": \n";
+            cout << "Facing" << history << ": \n";
 
         for (int card = 0; card < NUM_HANDS; card++) {
             cout << "\tWith " << card << ":\n";
@@ -158,17 +171,21 @@ struct InfoSets {
             }
 
             for (int child_idx = 0; child_idx < num_children; child_idx++) {
-                const float pct = (get_regr(card, child_idx) / sum_regr) / 100.0;
+                const float pct = (get_regr(card, child_idx) / sum_regr) * 100.0;
 
-                switch (child_states[child_idx]) {
+                switch (child_states[ child_idx ]) {
                 case Type::Fold:
-                    cout << "Fold " << pct << "% of the time\n";
+                    cout << "\t\tFold " << pct << "% of the time\n";
                     break;
                 case Type::Showdown:
-                    cout << "Call " << pct << "% of the time\n";
+                    cout << "\t\tCall " << pct << "% of the time\n";
                     break;
                 case Type::Continue:
-                    cout << "Bet sz " << child_idx << " " << pct << "% of the time\n";
+                    // special case for checking
+                    if (child_idx == 0 && history == "")
+                        cout << "\t\tCheck " << child_idx << " " << pct << "% of the time\n";
+                    else
+                        cout << "\t\tBet sz " << child_idx << " " << pct << "% of the time\n";
                     break;
                 }
             }
@@ -176,12 +193,19 @@ struct InfoSets {
 
         cout << "\n";
 
+
         for (int child_idx = 0; child_idx < num_children; child_idx++) {
-            if (child_states[child_idx] != Type::Continue) continue;
+            if (child_states[ child_idx ] != Type::Continue) continue;
 
-            const std::string next_history = history + " bet " + std::to_string(child_idx);
+            std::string next_history;
 
-            children[child_idx]->print_strat(next_history);
+            // special case history for a check
+            if (child_idx == 0 && history == "")
+                next_history = history + " check";
+            else
+                next_history = history + " bet " + std::to_string(child_idx);
+
+            children[ child_idx ]->print_strat(next_history);
         }
     }
 };
@@ -209,12 +233,12 @@ public:
     // p1_to_act: whether p1 or p2 are acting in this state
     // rem_bets: deque of the remaining rounds of bet szs (all defined in terms of % of pot)
     InfoSets* make_child(float p1_mip, float p2_mip, bool p1_to_act, const std::deque<Bet>& rem_bets) {
-        InfoSets* ret = new InfoSets;
+        InfoSets* ret = new InfoSets(p1_to_act);
 
         const float mip = p1_mip + p2_mip + ante;
 
         // terminal state: fold
-        const float fold_util = p1_to_act ? -1.0 * p1_mip - ante : p2_mip + ante;
+        const float fold_util = p1_to_act ? p1_mip + ante : p2_mip + ante;
         ret->add_fold(fold_util);
 
         // all-in catch case, can only call or fold and the util is clipped on a call
@@ -228,15 +252,21 @@ public:
         const float call_util = p1_to_act ? p2_mip + ante : p1_mip + ante;
         ret->add_showdown(call_util);
 
-        const Bet& this_bets = rem_bets.front();
+        static const Bet empty_bets;
+        const Bet& this_bets = rem_bets.size() ? rem_bets.front() : empty_bets;
 
         // this is quite inefficient, but this is the cold path anyways
         auto next_round_rem_bets = rem_bets;
+
+        // no more bets, only terminal states
+        if (next_round_rem_bets.empty())
+            return ret;
+
         next_round_rem_bets.pop_front();
 
         // bet case(s)
         for (int bet_idx = 0; bet_idx < this_bets.size(); bet_idx++) {
-            const float next_bet = mip * this_bets[bet_idx];
+            const float next_bet = mip * this_bets[ bet_idx ];
 
             InfoSets* child;
 
@@ -254,20 +284,25 @@ public:
     // special case: facing a check, you can't fold
     // must be p2 to act
     InfoSets* make_child_no_fold(const std::deque<Bet>& all_bets) {
-        InfoSets* ret = new InfoSets;
+        InfoSets* ret = new InfoSets(false);
 
         // terminal state: check-through
         ret->add_showdown(ante);
 
         const Bet& this_bets = all_bets.front();
 
-        // this is quite inefficient, but this is the cold path anyways
         auto rem_bets = all_bets;
+
+        // no non-terminal child states
+        if (rem_bets.empty())
+            return ret;
+
+        // this is quite inefficient, but this is the cold path anyways
         rem_bets.pop_front();
 
         // bet case(s)
         for (int bet_idx = 0; bet_idx < this_bets.size(); bet_idx++) {
-            const float next_bet = ante * this_bets[bet_idx];
+            const float next_bet = ante * this_bets[ bet_idx ];
             InfoSets* child = make_child(0.0, next_bet, true, rem_bets);
             ret->add_child(child);
         }
@@ -278,22 +313,27 @@ public:
     // special case: under the gun, you can't fold and a check isn't terminal
     // must be p1 to act
     InfoSets* make_child_check_allowed(const std::deque<Bet>& all_bets) {
-        InfoSets* ret = new InfoSets;
+        InfoSets* ret = new InfoSets(true);
 
         // no immediate terminal states to create
 
         const Bet& this_bets = all_bets.front();
 
-        // this is quite inefficient, but this is the cold path anyways
         auto rem_bets = all_bets;
-        rem_bets.pop_front();
 
         // check case
         ret->add_child(make_child_no_fold(all_bets));
 
+        // no bets to consider
+        if (rem_bets.empty())
+            return ret;
+
+        // this is quite inefficient, but this is the cold path anyways
+        rem_bets.pop_front();
+
         // bet case(s)
         for (int bet_idx = 0; bet_idx < this_bets.size(); bet_idx++) {
-            const float next_bet = ante * this_bets[bet_idx];
+            const float next_bet = ante * this_bets[ bet_idx ];
             InfoSets* child = make_child(next_bet, 0.0, false, rem_bets);
             ret->add_child(child);
         }
@@ -316,7 +356,7 @@ public:
     float train_state(int card1, int card2, float prob) {
         assert(card1 != card2);
 
-        const bool p1_wins = hand_rankings[card1] > hand_rankings[card2];
+        const bool p1_wins = hand_rankings[ card1 ] > hand_rankings[ card2 ];
 
         return root->get_util(card1, card2, p1_wins, prob);
     }
@@ -329,7 +369,7 @@ public:
             for (int card2 = 0; card2 < NUM_HANDS; card2++) {
                 if (card1 == card2) continue;
 
-                const float prob = starting_prob1[card1] * starting_prob2[card2];
+                const float prob = starting_prob1[ card1 ] * starting_prob2[ card2 ];
 
                 util += prob * train_state(card1, card2, prob);
             }
--- dynamic_round/runner.cc
+++ dynamic_round/runner.cc
@@ -12,23 +12,26 @@ int main() {
 
     GameDef game_structure = { bets1 };
 
-    std::array<float, NUM_HANDS> starting_prob;
-    starting_prob.fill(1.0 / NUM_HANDS);
+    std::array<float, NUM_HANDS> p;
+    p.fill(1.0 / NUM_HANDS);
 
     std::array<int, NUM_HANDS> hand_ranks;
     for (int i = 0; i < NUM_HANDS; i++)
-        hand_ranks[i] = i;
+        hand_ranks[ i ] = i;
 
     const float ante = 1.0, stack_sz = 10.0;
 
     Game g(
-        starting_prob,
-        starting_prob,
+        p,
+        p,
         hand_ranks,
         ante,
         stack_sz,
         game_structure
     );
+
+    for (int i = 0; i < 10000; i++)
+        g.train();
 
     g.print();
 }
